827a8f011608afb30c4c471739fb03770bf38668
Ludmila Cisneros <ludmilacisneros97@hotmail.com>
2020-11-17 01:16:15 -0300

    Se agrega TP2 completo

---
+++ RecuperatoriosTP/TP2/TP-02/Entidades/Vehiculo.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        #region "Enumerados"
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
+        }
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+        #endregion
+
+        #region "Atributos"
+
+        EMarca marca;
+        string chasis;
+        ConsoleColor color;
+
+        #endregion
+
+        #region "Constructor"
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Vehiculo(EMarca marca, string chasis, ConsoleColor color)
+        {
+            this.marca = marca;
+            this.chasis = chasis;
+            this.color = color;
+        }
+
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        public abstract ETamanio Tamanio { get; }
+
+        #endregion
+
+        #region "Métodos"
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("CHASIS: {0}\r\n", this.chasis);
+            sb.AppendFormat("MARCA : {0}\r\n", this.marca.ToString());
+            sb.AppendFormat("COLOR : {0}\r\n", this.color.ToString());
+            sb.AppendFormat("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+        #region "Operadores"
+        public static explicit operator string(Vehiculo p)
+        {
+            string mensaje = p.Mostrar();
+
+            return mensaje;
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1.chasis == v2.chasis);
+        }
+        #endregion
+    }
+}
